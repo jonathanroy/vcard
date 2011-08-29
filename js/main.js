7faf2d26698474e81e74eb56e3525493908fe3a6
Jonathan Roy <mail@jonathan-roy.com>
2011-08-28 22:07:29 -0400

    Simply making everything work...

--- js/main.js
+++ js/main.js
@@ -17,6 +17,12 @@ $(document).ready(function() {
 	$name = $master.find('#name');
 	$info = $master.find('#info');
 
+	// fade in / fade out links on hover (including social media icons)
+	$('a').fadeTo(0, 0.618).hover(
+		function() { $(this).stop(true,true).fadeTo(250, 1.000); },
+		function() { $(this).stop(true,true).fadeTo(250, 0.618); }
+	);
+
 });
 
 $(window).load(function() {
