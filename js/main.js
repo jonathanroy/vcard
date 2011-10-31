6c2e0c820a2ca6b6465b33547413f63cffd25a9f
Jonathan Roy <jroy@optimumweb.ca>
2011-10-31 20:02:44 +0000

    Fix GATC

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-// Trigger Google Analytics
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-6167339-30']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-
 $(document).ready(function() {
 
 	// define objects
