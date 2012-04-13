4f3d9a1341357710383095cb19afd8428e449f45
Jonathan Roy <jroy@optimumweb.ca>
2012-04-12 23:22:33 -0400

    Update and clean

--- js/main.js
+++ js/main.js
@@ -1,34 +1,37 @@
 $(document).ready(function() {
 
 	// define objects
-	$viewport = $(window);
-	$master = $('#master');
-	$name = $master.find('#name');
-	$info = $master.find('#info');
+	var $window = $(window);
+	var $master = $('#master');
+	var $name = $master.find('#name');
+	var $info = $master.find('#info');
+	var $anchor = $master.find('a');
 
 	// fade in / fade out links on hover (including social media icons)
-	$('a').fadeTo(0, 0.618).hover(
+	$anchor.fadeTo(0, 0.618).hover(
 		function() { $(this).stop(true,true).fadeTo(250, 1.000); },
 		function() { $(this).stop(true,true).fadeTo(250, 0.618); }
 	);
+	
+	$window.load(function() {
 
-});
+		// make $name and $info same height
+		var maxHeight = Math.max( $name.height(), $info.height() );
+		$name.height( maxHeight );
+		$info.height( maxHeight );
+	
+	});
+	
+	// center align $master in $viewport on window load and resize
+	$window.bind('load resize', function() {
+		vCenter($master, $viewport);
+	});
 
-$(window).load(function() {
-
-	// make $name and $info same height
-	maxHeight = Math.max( $name.height(), $info.height() );
-	$name.height( maxHeight );
-	$info.height( maxHeight );
-
-});
-
-// center align $master in $viewport on window load and resize
-$(window).bind('load resize', function() {
-	vCenter($master, $viewport);
 });
 
 function vCenter($object, $reference) {
-	$object.css('top', ( $reference.height() - $object.outerHeight(true) ) / 2);
+	if ( $object.size() && $reference.size() ) {
+		$object.css('top', ( $reference.height() - $object.outerHeight(true) ) / 2);
+	}
 }
 
